3e200a650ea6cc0c155e7e9a11177278d7de3680
AndriiSameliuk <andrii.sameliuk@webspellchecker.net>
2020-01-29 17:32:54 +0200

    Add method which set WebSpellChecker theme

--- src/wproofreader.js
+++ src/wproofreader.js
@@ -25,6 +25,7 @@ export default class WProofreader extends Plugin {
 		];
 
 		this._userOptions = this._getUserOptions();
+		this._setTheme();
 
 		this._loadWscbundle()
 			.then(() => {
@@ -59,6 +60,16 @@ export default class WProofreader extends Plugin {
 		}
 
 		return config;
+	}
+
+	/**
+	 * Checks if the theme option exists otherwise sets gray theme.
+	 * @private
+	 */
+	_setTheme() {
+		if (!this._userOptions.theme) {
+			this._userOptions.theme = 'gray';
+		}
 	}
 
 	/**
