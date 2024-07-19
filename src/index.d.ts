fe50e633cfa89603848c1c02d187fa08b5606ac5
Bohdan <nabogch@gmail.com>
2024-07-19 15:54:44 +0200

    Define path to module types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,7 @@
 import { Plugin } from '@ckeditor/ckeditor5-core';
 
-declare module '@webspellchecker/wproofreader-ckeditor5/src/wproofreader' {
-	export default class WProofreader extends Plugin {}
-}
+export class WProofreader extends Plugin {}
+
 declare module '@ckeditor/ckeditor5-core' {
 	interface EditorConfig {
 		wproofreader?: Object;
